44eabbb85d6171bdd86e65349e065eff24d83b21
vicky <s353397@sinyi.com.tw>
2019-07-24 17:49:33 +0800

    - 實作 index page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap">
+      <div className="index-menu">
+        <div className="index-menu-box">
+          <button className="start-button">START</button>
+          <div className="caption">How to play?</div>
+        </div>
+      </div>
     </div>
   );
 }
