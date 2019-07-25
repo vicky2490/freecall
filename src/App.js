6aea1562d39d46c5c1d9b9f9c4729966b3cf6f3c
vicky <s353397@sinyi.com.tw>
2019-07-25 10:38:48 +0800

    add router to game add game page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="wrap">
       <div className="index-menu">
         <div className="index-menu-box">
-          <button className="start-button">START</button>
+          <Link to="/game"><button className="start-button">START</button></Link>
           <div className="caption">How to play?</div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './css/cssReset.css';
+import { Switch, Router, Route } from 'react-router';
+import { createHashHistory } from 'history';
 import App from './App';
+import Game from './components/gameIndex/gameIndex';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createHashHistory();
+
+ReactDOM.render((
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/" component={App}/>
+      <Route path="/game" component={Game}/>
+    </Switch>
+  </Router>
+  ), document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
