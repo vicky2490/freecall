9d50b6dbb19e0382f0d49442f71458acc29e4298
vicky2490 <s353397@sinyi.com.tw>
2019-07-26 07:19:28 +0800

    add vacancy and standard box.

--- src/components/gameIndex/gameIndex.js
+++ src/components/gameIndex/gameIndex.js
@@ -13,6 +13,28 @@ class GameIndex extends Component {
 
     return (
       <div className="game-wrap">
+        <div className="header">
+          <div className="title">FREECELL</div>
+          <div className="count-bar">
+            <div className="count-time-move">TIME:</div>
+            <div className="count-time-move count-num">00:00</div>
+            <div className="count-time-move">MOVE:</div>
+            <div className="count-time-move count-num">00</div>
+          </div>
+        </div>
+        <div className="context">
+          <div className="vacancy-box"></div>
+          <div className="vacancy-box"></div>
+          <div className="vacancy-box"></div>
+          <div className="vacancy-box"></div>
+          <div className="standard-box suithearts">
+            {/*<div className="diamonds"></div>*/}
+          </div>
+          <div className="standard-box suitdiamonds"></div>
+          <div className="standard-box suitclubs"></div>
+          <div className="standard-box suitspades"></div>
+        </div>
+        
       </div>
     )
   }
